2039c405069e986822fbc5ede3cf280ea4c3ff9d
Gursheyss <rkwarya@gmail.com>
2023-05-26 20:39:37 -0700

    Made dm ephemeral to hide content of command, probably not best way but works for now

--- commands/main/orderCheckCommand.js
+++ commands/main/orderCheckCommand.js
@@ -79,7 +79,8 @@ module.exports = {
             }
             else if (interactionType == 'dm') {
                 interaction.user.send({ embeds: [embed] })
-                await interaction.reply('DM sent!')
+                await interaction.reply({ content: `Successfully DM\'d order data to ${interaction.user}`, ephemeral: true });
+                await interaction.followUp(`Successfully DM\'d order data to ${interaction.user})`)
                 console.log(`Successfully sent dm to ${interaction.user}`)
             }
             else {
